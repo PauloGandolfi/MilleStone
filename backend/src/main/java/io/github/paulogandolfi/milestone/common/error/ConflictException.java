f592c8b9e79bde223ae88fab66a092cdf3099975
Paulo Gandolfi <133063347+PauloGandolfi@users.noreply.github.com>
2026-09-03 08:27:47 -0300

    feat: add conflict exception

---
+++ backend/src/main/java/io/github/paulogandolfi/milestone/common/error/ConflictException.java
@@ -0,0 +1,8 @@
+package io.github.paulogandolfi.milestone.common.error;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+}
